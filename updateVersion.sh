9972e93018dfce08ddd95f679be8409fdab53010
Kai Schwarz <kschwarz@hexonet.net>
2021-12-09 14:49:13 +0100

    fix(semantic-release): added missing configuration file

--- updateVersion.sh
+++ updateVersion.sh
@@ -8,5 +8,4 @@
 newversion="$1"
 
 printf -v sed_script 's/return "[0-9]\+\.[0-9]\+\.[0-9]\+"/return "%s"/g' "${newversion}"
-sed -i -e "${sed_script}" src/HexonetClient.php
-sed -i -e "${sed_script}" src/RRPproxyClient.php
+sed -i -e "${sed_script}" src/HEXONET/Client.php
