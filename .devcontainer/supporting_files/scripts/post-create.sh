a749ca380205d8684dad24d34e216971ff31f886
Asif Nawaz <asif.nawaz@centralnic.com>
2022-12-14 17:26:34 +0000

    build(docker): Dev containerization with PHP 7.3 preinstalled

---
+++ .devcontainer/supporting_files/scripts/post-create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# NOTE: This file will be executed as remoteUser (devcontainer.json)
+echo "=> Script: post-create.sh Executed by: $(whoami)"
+
+sudo npm install --silent --progress=false --global gulp-cli commitizen@latest cz-conventional-changelog@latest semantic-release-cli@latest
+
+# shellcheck source=/dev/null
+source ~/.zshrc
+
+# install composer deps
+composer install
+npm ci
+
+exit 0
