048b331a22bfa3ccf413ef71536e94c22aa6fbf7
Asif Nawaz <asif.nawaz@centralnic.com>
2024-03-25 09:25:07 +0000

    perf(client.php & sessionclient.php): reviewed IDN Conversion with php-idna-translator plugin

--- src/CNR/SessionClient.php
+++ src/CNR/SessionClient.php
@@ -9,6 +9,8 @@
 
 namespace CNIC\CNR;
 
+use CNIC\IDNA\Factory\ConverterFactory;
+
 /**
  * CNR API Client
  *
@@ -73,42 +75,7 @@ class SessionClient extends \CNIC\HEXONET\SessionClient
      */
     public function IDNConvert($domains)
     {
-        $results = [];
-        foreach ($domains as $idx => $d) {
-            $results[$idx] = [
-                "PUNYCODE" => $d,
-                "IDN" => $d
-            ];
-        }
-        if ($this->settings["needsIDNConvert"]) {
-            foreach ($domains as $idx => $domain) {
-                $nontransitional = (bool)preg_match("/\.(be|ca|de|fr|pm|re|swiss|tf|wf|yt)\.?$/i", $domain);
-                $tmp = idn_to_ascii(
-                    $domain,
-                    ($nontransitional) ?
-                        IDNA_NONTRANSITIONAL_TO_ASCII :
-                        IDNA_DEFAULT,
-                    INTL_IDNA_VARIANT_UTS46
-                );
-                if ($tmp === false) {
-                    continue;
-                }
-                if (preg_match("/xn--/", $tmp)) {
-                    $results[$idx]["PUNYCODE"] = $tmp;
-                }
-                $tmp = idn_to_utf8(
-                    $results[$idx]["PUNYCODE"],
-                    ($nontransitional) ?
-                        IDNA_NONTRANSITIONAL_TO_ASCII :
-                        IDNA_DEFAULT,
-                    INTL_IDNA_VARIANT_UTS46
-                );
-                if (!empty($tmp)) {
-                    $results[$idx]["IDN"] = $tmp;
-                }
-            }
-        }
-        return $results;
+        return ConverterFactory::convert($domains);
     }
 
     /**
@@ -152,7 +119,7 @@ class SessionClient extends \CNIC\HEXONET\SessionClient
         if (!empty($toconvert)) {
             $results = $this->IDNConvert($toconvert);
             foreach ($results as $idx => $row) {
-                $cmd[$idxs[$idx]] = $row["PUNYCODE"];
+                $cmd[$idxs[$idx]] = $row["punycode"];
             }
         }
         return $cmd;
